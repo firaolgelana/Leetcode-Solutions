ea762785704a8627d6c9fd1b934594963f6d12fe
firaolgelana <gelanafiraol@gmail.com>
2025-12-27 20:13:02 +0300

    Time: 209 ms (32.24%), Space: 51.4 MB (79.36%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        min_heap = []
+        empty = list(range(n))
+        count = [0] * n
+        for i in range(len(meetings)):
+            start, end = meetings[i]
+            while min_heap and min_heap[0][0] <= start:
+                top = heappop(min_heap)
+                heappush(empty, top[1])
+            if not empty:
+                top = heappop(min_heap)
+                heappush(min_heap, (end + top[0] - start, top[1]))
+                count[top[1]] += 1
+            else:
+                room = heappop(empty)
+                heappush(min_heap, (end, room))
+                count[room] += 1
+
+        return count.index(max(count))
+        
