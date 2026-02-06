a85d2ea0656ea41418227d7bd3b4fb387f3a72b5
firaolgelana <gelanafiraol@gmail.com>
2026-02-06 20:35:59 +0300

    Time: 115 ms (92.06%), Space: 34.7 MB (28.52%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minRemoval(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        n = len(nums)
+        l = best = 0
+
+        for r in range(n):
+            while nums[r] > nums[l] * k:
+                l += 1
+            best = max(best, r - l + 1)
+
+        return n - best
