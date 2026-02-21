714c705d33f45802e037d1b30b00593cdc48fb18
firaolgelana <gelanafiraol@gmail.com>
2026-02-21 17:44:25 +0300

    Time: 123 ms (51.5%), Space: 19.5 MB (14.21%) - LeetHub

---
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        def is_prime(n):
+            if n <= 1:
+                return False
+            for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                    return False
+            return True
+
+        count = 0
+        for num in range(left, right + 1):
+            bits = num.bit_count()
+            count += is_prime(bits)
+
+        return count
